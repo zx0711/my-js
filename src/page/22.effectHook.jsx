e1242d043071f9768278341f6433295a83a5ed88
xiao.zhang <xiao.zhang@ximalaya.com>
2020-09-29 18:03:26 +0800

    fix

--- src/page/22.effectHook.jsx
+++ src/page/22.effectHook.jsx
@@ -3,7 +3,7 @@
  * @Author: xiao.zhang
  * @Date: 2020-09-28 23:09:47
  * @LastEditors: xiao.zhang
- * @LastEditTime: 2020-09-29 14:03:15
+ * @LastEditTime: 2020-09-29 17:56:36
  */
 import React, { useState, useEffect, useRef } from 'react'
 
