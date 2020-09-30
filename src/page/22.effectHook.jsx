f05b77f0ab4316984d9ff23d4309469357d4835c
xiao.zhang <xiao.zhang@ximalaya.com>
2020-09-30 10:26:22 +0800

    fix

--- src/page/22.effectHook.jsx
+++ src/page/22.effectHook.jsx
@@ -3,7 +3,7 @@
  * @Author: xiao.zhang
  * @Date: 2020-09-28 23:09:47
  * @LastEditors: xiao.zhang
- * @LastEditTime: 2020-09-29 17:56:36
+ * @LastEditTime: 2020-09-29 19:31:32
  */
 import React, { useState, useEffect, useRef } from 'react'
 
@@ -12,7 +12,7 @@ function ErrorDemo() {
   const dom = useRef(null)
   useEffect(() => {
     dom.current.addEventListener('click', () => setCount(count + 1))
-  }, [count])
+  }, [])
   return <div ref={dom}>{count}</div>
 }
 
